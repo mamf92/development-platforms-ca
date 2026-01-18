d6ec6b22f6fb85c8a689424fbb4314afb6dddd0f
Martin Fischer <115947567+mamf92@users.noreply.github.com>
2026-01-18 20:44:37 +0100

    Fix ESLint config.

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: globals.node,
+      parser: tsParser,
+      parserOptions: { project: "./tsconfig.json" },
+    },
+    plugins: { "@typescript-eslint": tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...tsPlugin.configs["recommended-requiring-type-checking"].rules,
+    },
+  },
+  prettier,
+];
